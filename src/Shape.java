cd74597b9b2f9e5b77bd72529fdf8cf28caf7fb8
Kwright6 <usaf633@yahoo.com>
2024-01-19 13:15:33 -0800

    In Progress: Part 1

---
+++ src/Shape.java
@@ -0,0 +1,13 @@
+// TODO: Declare 4 class constraints for the Shape class as comments
+/* Constraints:
+    - x and y must be integers
+    - x and y must be non-negative
+    - the color must be a java default?
+    - limit the size?
+ */
+
+public class Shape {
+    // TODO: 1.2 Data Fields
+    // TODO: 1.3 Constructors
+    // TODO: 1.4 Instance Methods
+}
