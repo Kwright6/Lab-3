b36ca06a5cfcdcb0e8bcc8f5f6444bb46023ced7
Kwright6 <usaf633@yahoo.com>
2024-01-21 03:37:01 -0800

    In Progress: Part 4

---
+++ src/Fraction.java
@@ -0,0 +1,16 @@
+// TODO: 4.1 Class Constraints
+/*
+Class Constraints
+   -
+ */
+public class Fraction {
+    // TODO: 4.2 Data Fields
+
+
+
+    // TODO: 4.3 Constructor
+
+
+
+    // TODO: 4.4 Methods
+}
