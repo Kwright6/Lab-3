48c2c7ffc1436d8f35f90ed1822033c2bef12d5a
Kwright6 <usaf633@yahoo.com>
2024-01-20 19:48:12 -0800

    In Progress: Part 1 Done

--- src/Shape.java
+++ src/Shape.java
@@ -1,11 +1,11 @@
 import java.awt.Color;
+import java.awt.Graphics;
 
-// TODO: Declare 4 class constraints for the Shape class as comments
 /* Constraints:
     - x and y must be integers
     - x and y must be non-negative
+    - x and y must be lower than 1000
     - the color must be a java default?
-    - limit the size?
  */
 
 public class Shape {
@@ -34,7 +34,7 @@ public class Shape {
         color = other.color;
     }
 
-    // TODO: 1.4 Instance Methods
+    // Instance Methods
     public int getX() {
         return x;
     }
@@ -51,16 +51,25 @@ public class Shape {
         this.y = y;
     }
 
-    // TODO: public String toString();
-    // Returns a string that describes the x and y coordinates and the color of this shape.
-    // For example, a blue shape at (10, 200) should return "Shape (10, 200) blue"
+    public String toString() {
+        // Returns a string that describes the x and y coordinates and the color of this shape.
+        // For example, a blue shape at (10, 200) should return "Shape (10, 200) blue"
 
-    // TODO: public double getArea();
-    // Since we can't compute the area of the shape without knowing what kind of shape it is, this method should just return -1.
-    // Subclasses will override this method to return a useful value.
+        return "Shape (" + this.x + ", " + this.y + ") " + this.color;
+    }
 
-    // TODO: public void draw(Graphics g);
+    // Since we can't compute the area of the shape without knowing
+    // what kind of shape it is, this method should just return -1.
+    public double getArea() {
+        // Since we can't compute the area of the shape without knowing
+        // what kind of shape it is, this method should just return -1.
+
+        return -1;  // Subclasses will override this method to return a useful value.
+    }
+
     // This method takes a Graphics object as an argument.
-    // Later, we will learn how to use the Graphics object to draw shapes, but for now, the body of the method will be empty
-
+    public void draw(Graphics g) {
+        // Later, we will learn how to use the Graphics object to draw shapes,
+        // but for now, the body of the method will be empty
+    }
 }
