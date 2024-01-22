8a1c0962b10310317afdefb89c0dc2ca69a213db
Kwright6 <usaf633@yahoo.com>
2024-01-22 01:12:57 -0800

    In Progress: Part 4

--- src/Fraction.java
+++ src/Fraction.java
@@ -11,29 +11,37 @@ public class Fraction {
     public final double numerator; // Q: Why don’t we make this data private? A: So it can be read in another class
     public final double denominator; // Q: What data types should these items be? A: double or float if we want more precision
 
-    // TODO: 4.3 Constructors
+    // Constructor
     public Fraction(double numerator, double denominator) {
         this.numerator = numerator;
         this.denominator = denominator;
     }
-    // todo: A copy constructor that takes a Fraction object as an argument
-    //  Give this fraction the same numerator and denominator as the argument fraction.
+    // Copy Constructor
     public Fraction(Fraction other) {
         numerator = other.numerator;
         denominator = other.denominator;
     }
 
+    public String toString() {
+        return this.numerator + "\\" + this.denominator;
+    }
 
     // TODO: 4.4 Methods
-    // todo: public String toString()
-    // todo: public Fraction add(Fraction that)
-    //Returns a new fraction which is the sum of this fraction and the argument fraction.
-    //This method is tricky! Review how to add fractions, and consider writing helper methods which you can call in add.
-    // todo: public boolean equals(Object other)
-    /* - Indicates whether this fraction is equal to the specified fraction.
-       - Since Fraction is a subclass of Object, it inherits an equals method from Object which takes an Object as an argument.
-            - Our new method will override the Object method, so our method has to take an Object argument as well.
-            - So, we start the method by making sure the argument is a non-null Fraction, and if so, we cast the argument from Object to Fraction.
-            - Then, we compare the numerators and denominators of the fractions.
-     */
+    public Fraction add(Fraction that) {
+        // todo:
+        //      Returns a new fraction which is the sum of this fraction and the argument fraction.
+        //      This method is tricky! Review how to add fractions, and consider writing helper methods which you can call in add.
+        return new Fraction(0,0);  // todo: placeholder
+    }
+
+    public boolean equals(Object other) {
+        // todo:
+        /*  - Indicates whether this fraction is equal to the specified fraction.
+            - Since Fraction is a subclass of Object, it inherits an equals method from Object which takes an Object as an argument.
+                - Our new method will override the Object method, so our method has to take an Object argument as well.
+                - So, we start the method by making sure the argument is a non-null Fraction, and if so, we cast the argument from Object to Fraction.
+                - Then, we compare the numerators and denominators of the fractions.
+         */
+        return false;
+    }
 }
